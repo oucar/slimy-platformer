43cb8254dec56843286bcf80b510c777421d04b8
Onur Ucar <onurucar@MacBook-Pro-4.attlocal.net>
2022-04-30 14:17:10 -0400

    polishing the game feel

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 var gravity = 300
 var velocity = Vector2.ZERO
 var maxHorizontalSpeed = 100
+var horizontalAcceleration = 1500
 var jumpSpeed = 200
 
 # Called when the node enters the scene tree for the first time.
@@ -19,7 +20,13 @@ func _process(delta):
 	moveVector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	moveVector.y = -1 if  Input.is_action_just_pressed("jump") else 0 
 	
-	velocity.x = moveVector.x * maxHorizontalSpeed
+	velocity.x += moveVector.x * horizontalAcceleration * delta
+	# deacceleration
+	if(moveVector.x == 0):
+		# https://www.gamedeveloper.com/programming/improved-lerp-smoothing-
+		velocity.x = lerp(velocity.x, 0, .1)
+	
+	velocity.x = clamp(velocity.x, -maxHorizontalSpeed, maxHorizontalSpeed)
 	
 	# we want to jump
 	if (moveVector.y < 0 && is_on_floor()):
