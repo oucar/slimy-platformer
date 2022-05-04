4996b1293130e08d6e20ab3ce1dda9b313ec1a66
oucar <hello@ucaronur.com>
2022-05-03 22:06:45 -0400

    counter added to the ui

---
+++ scenes/UI/LevelUI.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+func _ready():
+	# Connect to the signals in the base level scene
+	var baseLevels = get_tree().get_nodes_in_group("base_level")
+	
+	# Doesn't required for the production unless you run this scene.
+	if(baseLevels.size() > 0):
+		print(baseLevels.size())
+		baseLevels[0].connect("coin_total_changed", self, "on_coin_total_changed")
+		baseLevels[0].connect("enemy_total_changed", self, "on_enemy_total_changed")
+	
+func on_coin_total_changed(totalCoins, collectedCoins):
+	get_node("MarginContainer/CoinLabel").text = str(collectedCoins, "/", totalCoins)
+	
+func on_enemy_total_changed(totalEnemies, enemiesKilled):
+	get_node("MarginContainer/EnemyLabel").text = str(enemiesKilled, "/", totalEnemies)
+	
