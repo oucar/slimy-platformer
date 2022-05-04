56ae8cd1479b4e740a6a09e63c948ef8b47c57a0
oucar <hello@ucaronur.com>
2022-05-04 19:36:55 -0400

    camera shake

--- scenes/Camera2D.gd
+++ scenes/Camera2D.gd
@@ -2,22 +2,60 @@ extends Camera2D
 
 var targetPosition = Vector2.ZERO
 export(Color, RGB) var backgroundColor
+# shaky camera
+export(OpenSimplexNoise) var shakeNoise
 
+var xNoiseSampleVector = Vector2.RIGHT
+var yNoiseSampleVector = Vector2.DOWN
+var xNoiseSamplePosition = Vector2.ZERO
+var yNoiseSamplePosition = Vector2.ZERO
+var noiseSampleTravelRate = 500
+var maxShakeOffset = 10
+var currentShakePercentage = 0
+var shakeDecay = 3
 
 func _ready():
 	# Change background color
 	VisualServer.set_default_clear_color(backgroundColor)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # LERK to make it look more professional
 func _process(delta):
 	get_target_position()
 	global_position = lerp(targetPosition, global_position, pow(2, -10 * delta))
+	
+	if (currentShakePercentage > 0):
+		xNoiseSamplePosition += xNoiseSampleVector * noiseSampleTravelRate * delta
+		yNoiseSamplePosition += yNoiseSampleVector * noiseSampleTravelRate * delta
+		var xSample = shakeNoise.get_noise_2d(xNoiseSamplePosition.x, xNoiseSamplePosition.y)
+		var ySample = shakeNoise.get_noise_2d(yNoiseSamplePosition.x, yNoiseSamplePosition.y)
+		
+		var calculatedOffset = Vector2(xSample, ySample) * maxShakeOffset * pow(currentShakePercentage, 2)
+		offset = calculatedOffset
+		
+		# we can use lerp to decay the camera shake
+		currentShakePercentage = clamp(currentShakePercentage - shakeDecay * delta, 0, 1)
 
-
+# Following the player with camera
 func get_target_position():
 	# get the player group
 	var players = get_tree().get_nodes_in_group("player")
 	if(players.size() > 0):
 		var player = players[0]
 		targetPosition = player.global_position
+
+# will be used later
+func get_target_position_from_node_group(groupName):
+	var nodes = get_tree().get_nodes_in_group(groupName)
+	if (nodes.size() > 0):
+		var node = nodes[0]
+		targetPosition = node.global_position
+		return true
+	return false
+
+# Shaky camera
+func apply_shake(percentage):
+	currentShakePercentage = clamp(currentShakePercentage + percentage, 0, 1)
+	
+	
