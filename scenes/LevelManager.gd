3cf27ff43180ab76b57087924e916def8bedb596
Onur Ucar <onurucar@iu.edu>
2022-05-04 14:21:47 -0400

    initial ui changes, level changing mechanics have been improved

--- scenes/LevelManager.gd
+++ scenes/LevelManager.gd
@@ -3,14 +3,17 @@ extends Node2D
 export(Array, PackedScene) var levelScenes
 
 var currentLevelIndex = 0
-
-func _ready():
-	change_level(currentLevelIndex)
 	
 func change_level(levelIndex):
+	
 	currentLevelIndex = levelIndex
+	
+	# In order to cycle endlessly between the levels, I might update this in the future
+	if(currentLevelIndex >= levelScenes.size()):
+		currentLevelIndex = 0
+
 	# Click on LevelManager node and inspect the given array
-	get_tree().change_scene(levelScenes[levelIndex].resource_path)
+	get_tree().change_scene(levelScenes[currentLevelIndex].resource_path)
 
 func increment_level():
 	change_level(currentLevelIndex + 1)
